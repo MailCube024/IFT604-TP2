b64b3e2f5f221cb44ed5d9c57938d2b4044efc02
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-06 15:33:35 -0400

    Initial commit

---
+++ gen/com/example/IFT604_tp2/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.IFT604_tp2;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}

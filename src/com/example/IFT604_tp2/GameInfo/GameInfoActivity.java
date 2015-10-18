69f9ea471b54c2fb1608588ea0a77f1dcf781109
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-18 10:53:28 -0400

    Added radio button click event

--- src/com/example/IFT604_tp2/GameInfo/GameInfoActivity.java
+++ src/com/example/IFT604_tp2/GameInfo/GameInfoActivity.java
@@ -2,6 +2,8 @@ package com.example.IFT604_tp2.GameInfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import com.example.IFT604_tp2.R;
 
 /**
@@ -14,4 +16,20 @@ public class GameInfoActivity extends Activity {
         setContentView(R.layout.match_summary);
     }
 
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        if (checked) {
+            switch (view.getId()) {
+                case R.id.optHost:
+                    if (checked)
+                        //TODO: Set bet to host
+                        break;
+                case R.id.optVisitor:
+                    if (checked)
+                        //TODO: Set bet to visitor
+                        break;
+            }
+        }
+    }
 }

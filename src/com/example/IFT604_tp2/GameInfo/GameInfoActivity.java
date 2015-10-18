2bb08de99896997ae57e0b5ca505da7f29e36b35
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-18 12:45:55 -0400

    Changed Application icon. Added basic handling of game description info in activity.

--- src/com/example/IFT604_tp2/GameInfo/GameInfoActivity.java
+++ src/com/example/IFT604_tp2/GameInfo/GameInfoActivity.java
@@ -1,19 +1,33 @@
 package com.example.IFT604_tp2.GameInfo;
 
+import HockeyLive.Common.Models.Game;
+import HockeyLive.Common.Models.Goal;
+import HockeyLive.Common.Models.Penalty;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import com.example.IFT604_tp2.IntentKeys;
 import com.example.IFT604_tp2.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Michaël on 10/17/2015.
  */
 public class GameInfoActivity extends Activity {
     private Intent currentIntent;
-    private int gameID;
+    private Game game;
+    private ArrayAdapter<Penalty> hostPenaltyAdapter;
+    private ArrayAdapter<Penalty> visitorPenaltyAdapter;
+    private ArrayAdapter<Goal> hostGoalAdapter;
+    private ArrayAdapter<Goal> visitorGoalAdapter;
+
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -21,7 +35,48 @@ public class GameInfoActivity extends Activity {
         setContentView(R.layout.game_summary);
 
         currentIntent = getIntent();
-        gameID = currentIntent.getIntExtra(IntentKeys.GAME_ID, 0);
+        Bundle bundle = currentIntent.getExtras();
+        if (bundle != null)
+            game = (Game) bundle.getSerializable(IntentKeys.GAME_ID);
+        else
+            game = new Game(0, "Chicago", "Montreal");
+
+        InitializeGameDescription();
+        InitializeAdapters();
+    }
+
+    private void InitializeGameDescription() {
+        ((TextView) findViewById(R.id.lblGameInfoTitle)).setText(game.toString());
+        ((TextView) findViewById(R.id.lblHostname)).setText(game.getHost());
+        ((TextView) findViewById(R.id.lblVisitorName)).setText(game.getVisitor());
+    }
+
+    private void InitializeAdapters() {
+        hostPenaltyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        hostGoalAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        visitorPenaltyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        visitorGoalAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+
+        ((ListView) findViewById(R.id.lstHostPenalties)).setAdapter(hostPenaltyAdapter);
+        ((ListView) findViewById(R.id.lstHostGoals)).setAdapter(hostPenaltyAdapter);
+        ((ListView) findViewById(R.id.lstVisitorPenalties)).setAdapter(visitorPenaltyAdapter);
+        ((ListView) findViewById(R.id.lstVisitorGoals)).setAdapter(visitorPenaltyAdapter);
+    }
+
+    public void UpdatePenaltyList(List<Penalty> hostPenalties, List<Penalty> visitorPenalties) {
+        hostPenaltyAdapter.clear();
+        visitorPenaltyAdapter.clear();
+
+        hostPenaltyAdapter.addAll(hostPenalties);
+        visitorPenaltyAdapter.addAll(visitorPenalties);
+    }
+
+    public void UpdateGoalList(List<Goal> hostGoals, List<Goal> visitorGoals) {
+        hostGoalAdapter.clear();
+        visitorGoalAdapter.clear();
+
+        hostGoalAdapter.addAll(hostGoals);
+        visitorGoalAdapter.addAll(visitorGoals);
     }
 
     public void onRadioButtonClicked(View view) {
